f62008334a37000689ab758aeb6505ee79ff18b7
Arsen <simf.arsenio@gmail.com>
2018-07-10 11:33:38 +0300

    signup fixed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -8,13 +8,12 @@ export default {
                 password: credentials.password })
                 .then(res => res.data),
 
-        signup: (data) => {
+        signup: (data) => 
             axios.post('/signup', {
                 login: data.login,
                 password: data.password,
                 email: data.email,
                 username: data.username })
                 .then(res => res.data)
-        }
     }
 }
